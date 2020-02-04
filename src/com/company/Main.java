5425706de00de9e443515c47cd259de7fb9da3dc
mr.Prog <mister_prog@mail.ru>
2020-02-04 21:21:30 +0300

    Schitaem a + b

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company; // базовый namespace - лучше задавать доменном-компании-наоборот
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world!");
+        int a = 5;
+        int b = 7;
+        System.out.println("Переменная а равна " + a + "\nПеременная b равна " + b
+                + "\nСумма a и b равна " + (a + b));
     }
 }
