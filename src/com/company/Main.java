0ffc54591212a032e5f4c18f0f0a53691f3db90c
mr.Prog <mister_prog@mail.ru>
2020-02-04 20:58:57 +0300

    Коммитим Hello world

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company; // базовый namespace - лучше задавать доменном-компании-наоборот
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world!");
+    }
+}
