28c000cfc50e80e8e04603f541e2d3ecdeec7ae1
mr.Prog <mister_prog@mail.ru>
2020-02-06 20:32:50 +0300

    Vozrast. Logical

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,23 +6,51 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String str = "Result: ";
-        char c = '%';
-        int a = 5;
-        int b = 7;
-        int r = test(5,7);
+//        String str = "Result: ";
+//        char c = '%';
+//        int a = 5;
+//        int b = 7;
+//        int r = test(5,7);
+//
+//        boolean cond = true;
+//        boolean notCond = !cond;
+//        boolean dynCond = a > b;
+//
+//        /*System.out.println("Переменная а равна " + a + "\nПеременная b равна " + b
+//                + "\nСумма a и b равна " + (a + b));
+//
+//        System.out.println("Теперь выводим возвращаемое значение метода sum: " + test(a,b));*/
 
-        boolean cond = true;
-        boolean notCond = !cond;
-        boolean dynCond = a > b;
+//
+//        if (a > b) {
+//            System.out.println("Значение переменной a БОЛЬШЕ значения переменной b");
+//        }
+//        else {
+//            System.out.println("Значение переменной a МЕНЬШЕ или РАВНО значения переменной b");
+//        }
 
-        /*System.out.println("Переменная а равна " + a + "\nПеременная b равна " + b
-                + "\nСумма a и b равна " + (a + b));
 
-        System.out.println("Теперь выводим возвращаемое значение метода sum: " + test(a,b));*/
 
-        System.out.println("dynCond = " + dynCond);
+        int age = 101;
 
+        System.out.println("Возраст человека: " + age + " года/лет");
+
+        if (age < 50)
+        {
+            System.out.println("Ребенок");
+        }
+        else if (age < 80)
+        {
+            System.out.println("Подросток");
+        }
+        else if (age < 100)
+        {
+            System.out.println("Взрослый");
+        }
+        else
+        {
+            System.out.println("Вот ты долгожитель!");
+        }
     }
 
    static int test (int a, int b)
