49b4f672ca7b659f59177db157c93c0555f1a58c
mr.Prog <mister_prog@mail.ru>
2020-02-06 20:12:10 +0300

    Second lesson. Data types

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,27 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String str = "Result: ";
+        char c = '%';
         int a = 5;
         int b = 7;
-        System.out.println("Переменная а равна " + a + "\nПеременная b равна " + b
+        int r = test(5,7);
+
+        boolean cond = true;
+        boolean notCond = !cond;
+        boolean dynCond = a > b;
+
+        /*System.out.println("Переменная а равна " + a + "\nПеременная b равна " + b
                 + "\nСумма a и b равна " + (a + b));
+
+        System.out.println("Теперь выводим возвращаемое значение метода sum: " + test(a,b));*/
+
+        System.out.println("dynCond = " + dynCond);
+
+    }
+
+   static int test (int a, int b)
+    {
+        return a + b;
     }
 }
