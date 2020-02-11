8f92a0d35a1ff20d5d6a468b7f8616674744fd94
mr.Prog <mister_prog@mail.ru>
2020-02-11 12:23:29 +0300

    Created files Less1, less2. Replaced some code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,70 +4,13 @@ import java.util.Scanner;
 
 public class Main {
 
-    enum Color { // перечисления
-        GREEN,
-        VIOLET,
-        RED,
-        GOLD
-    }
-
-//    { //видимость переменных - внутри фигурн. скобок
-//        int a = 6;
-//        String str = "Hello, Friend!";
-//        System.out.println (a);
-//    }
-
-    static void colorText(Color color) {
-        String colorText = "";
-        switch (color) {
-            case GREEN:
-                colorText = "Зелёный";
-                break;
-            case VIOLET:
-                colorText = "Фиолетовый";
-                break;
-            case RED:
-                colorText = "Красный";
-                break;
-            default:
-                colorText = "Голубой";
-        }
-        System.out.print("\nЦвет: " + colorText);
-    }
 
     public static void main(String[] args) {
-        // write your code here
-//        String str = "Result: ";
-//        char c = '%';
-//        int a = 5;
-//        int b = 7;
-//        int r = test(5,7);
-//
-//        boolean cond = true;
-//        boolean notCond = !cond;
-//        boolean dynCond = a > b;
-//
-//        /*System.out.println("Переменная а равна " + a + "\nПеременная b равна " + b
-//                + "\nСумма a и b равна " + (a + b));
-//
-//        System.out.println("Теперь выводим возвращаемое значение метода sum: " + test(a,b));
-
-//
-//        if (a > b) {
-//            System.out.println("Значение переменной a БОЛЬШЕ значения переменной b");
-//        }
-//        else {
-//            System.out.println("Значение переменной a МЕНЬШЕ или РАВНО значения переменной b");
-//        }
-        // ifAge(23);
-        // caseAge(2);
-        // colorText(Color.GOLD);
-        //votTakoeKino();
-        //colorText(Color.GREEN);
-        caseKino();
+        //ifElseKino();
+        switchCaseKino();
     }
 
-    static void votTakoeKino() {
+    static void ifElseKino() {
         Scanner in = new Scanner(System.in);
         System.out.print("Input a number (1-5): ");
         int score = in.nextInt();
@@ -92,7 +35,7 @@ public class Main {
         in.close();
     }
 
-    static void caseKino() {
+    static void switchCaseKino() {
         Scanner in = new Scanner(System.in);
         System.out.print("Input a number (1-5): ");
         int val = in.nextInt();
@@ -120,48 +63,5 @@ public class Main {
         }
         in.close();
     }
-    /*
-
-    static void ifAge(int age) {
-        System.out.println("Возраст человека: " + age + " года/лет");
-
-        if (age < 50) {
-            System.out.println("Ребенок");
-            System.out.println("Цвет одежды: зеленый");//1
-        } else if (age < 80) {
-            System.out.println("Подросток");
-            System.out.println("Цвет одежды: фиолетовый");//2
-        } else if (age < 100) {
-            System.out.println("Взрослый");
-            System.out.println("Цвет одежды: красный");//3
-        } else {
-            System.out.println("Вот ты долгожитель!");
-        }
-    }
-
-    static void caseAge(int colorCode) {
-        String colorText = "";
-        switch (colorCode) {
-            case 1:
-                colorText = "Зелёный";
-                break;
-            case 2:
-                colorText = "Фиолетовый";
-                break;
-            case 3:
-                colorText = "Красный";
-                break;
-            default:
-                colorText = "Голубой";
-        }
-        System.out.print("Цвет: " + colorText);
-    }
-
-    static int test(int a, int b) {
-        return a + b;
-    }
-*/
-
-
 }
 
