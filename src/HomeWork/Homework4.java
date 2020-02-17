73d2c5633740fb23167863e43657cdd34384de66
mr.Prog <mister_prog@mail.ru>
2020-02-17 16:59:57 +0300

    added MainRandom class, Random variants

--- src/HomeWork/Homework4.java
+++ src/HomeWork/Homework4.java
@@ -1,14 +1,13 @@
 package mr.Prog;
 
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Homework4 {
     public static void main(String[] args) {
         compicGame();
     }
 
-    //Проверяем, является ли введенное с клавиатуры значение целым числом
-    //Отлавливаем Exception, если не является
     public static boolean isInt(String string) {
         try {
             Integer.parseInt(string);
@@ -18,12 +17,12 @@ public class Homework4 {
         return true;
     }
 
-    //Основной метод - диалог компа и человека
     public static void compicGame() {
         System.out.println("Привет! Я - Компик =) Давай поиграем?\n\nЯ загадал целое число от 1 до 100\nПопробуй отгадать, а я подскажу ;)");
 
-        int compicNumber = numGen(1, 100);
+        int compicNumber = ThreadLocalRandom.current().nextInt(1, 101);
         int humanNumber = waitInt();
+        int counter = 1;
 
         while (humanNumber != compicNumber) {
             if (humanNumber > compicNumber) {
@@ -32,13 +31,12 @@ public class Homework4 {
                 System.out.println("Моё число больше");
             }
             humanNumber = waitInt();
+            counter++;
         }
-        System.out.println("Ура! Ты угадал!\nМоё число " + compicNumber);
+        System.out.println("Ура! Ты угадал!\nМоё число " + compicNumber
+                + "\n\nКоличество твоих попыток: " + counter);
     }
 
-    //Запрашиваем ввод целого числа
-    //Проверяем, число ли это
-    //если число, парсим в int, возвращаем int
     public static int waitInt() {
         Scanner in = new Scanner(System.in);
 
@@ -48,13 +46,6 @@ public class Homework4 {
             System.out.println("Вы ввели число некорректно!\nПожалуйста, повторите ввод");
             inputString = in.nextLine();
         }
-        // in.close();
         return Integer.parseInt(inputString);
-    }
-
-    //Генерим рандомное число в заданном диапазоне
-    //Приводим число к int и возвращаем
-    public static int numGen(int min, int max) {
-        return min + (int) (Math.random() * max);
     }
 }
---
+++ src/Lesson4/MainRandom.java
@@ -0,0 +1,43 @@
+package Lesson4;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MainRandom {
+    public static void main(String[] args) {
+        //варианты вызова Random от min до max
+
+        //threadGenNum(1, 100);
+        //mathGenNum(1, 100);
+        genNumVariant();
+    }
+
+
+    public static void mathGenNum(int min, int max) {
+        //Math.random()
+        int genNum;
+        for (int i = 0; i < 100; i++) {
+            genNum = (int) (Math.random() * (max - min + 1)) + min;
+            System.out.println("Рандомное число " + (i + 1) + " равно " + genNum);
+        }
+    }
+
+    public static void threadGenNum(int min, int max) {
+        //оптимальный ThreadLocalRandom.current().nextInt();
+        int threadRand;
+        for (int i = 0; i < 100; i++) {
+            threadRand = ThreadLocalRandom.current().nextInt(1, 101);
+            System.out.println("Рандомное число " + (i + 1) + " равно " + threadRand);
+        }
+    }
+
+    public static void genNumVariant() {
+        //new Random().nextInt(5); // [0...4] [min = 0, max = 4]
+        int genNumVar;
+
+        for (int i = 0; i < 500; i++) {
+            genNumVar = (new Random().nextInt(100) + 1); //по идее, от [0...99] + 1
+            System.out.println("Рандомное число " + (i + 1) + " равно " + genNumVar);
+        }
+    }
+}
