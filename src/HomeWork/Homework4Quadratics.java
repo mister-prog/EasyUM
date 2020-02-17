777f8e176559bcb3e0ab8f293770a840716bb23e
mr.Prog <mister_prog@mail.ru>
2020-02-17 19:09:51 +0300

    changed Cuadratics

--- src/HomeWork/Homework4Quadratics.java
+++ src/HomeWork/Homework4Quadratics.java
@@ -2,6 +2,9 @@ package HomeWork;
 //////////////////////////////////////////
 // Квадратное уравнение
 // a * x^2 + b * x + с == 0
+// Дискриминант
+//D = b^2 - 4 * a * c
+//
 // Вар-ты:
 // --- D > 0, два корня ---
 // x1 = -b + sqrt(b^2 - 4 * a * c) / 2 * a
@@ -28,19 +31,24 @@ public class Homework4Quadratics {
         double a = waitDouble();
         double b = waitDouble();
         double c = waitDouble();
+        System.out.println("Вы ввели: a = " + a + " b = " + b + " с = " + c);
+        System.out.println("\n");
 
-        double discrim = b * b + 4 * a * c;
+
+        double discrim = b * b - 4 * a * c;
+        System.out.println("\nДискриминант = " + discrim);
 
         if (discrim < 0) {
             System.out.println("Дискриминант меньше нуля => комплексные корни считать неохота =)");
         } else if (discrim > 0) {
-            double x1 = (-1 * b + Math.sqrt(discrim)) / 2 * a;
-            double x2 = (-1 * b - Math.sqrt(discrim)) / 2 * a;
+
+            double x1 = (-b + Math.sqrt(discrim)) / (2 * a);
+            double x2 = (-b - Math.sqrt(discrim)) / (2 * a);
 
             System.out.println("Дискриминант больше нуля => два искомых числа:\nx1 = " +  x1 + "\nx2 = " + x2);
 
         } else if (discrim == 0) {
-            double x = -1 * b / 2 * a;
+            double x = b / 2 * a;
             System.out.println("Дискриминант равен нулю => одно искомое число:\nx1 = x2 = " + x);
         }
     }
