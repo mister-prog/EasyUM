c689d3e0f5c63f845c06ce7ade9be9a5f3e6bc2d
mr.Prog <mister_prog@mail.ru>
2020-02-18 16:49:27 +0300

    Homework4Ranges finished

--- src/HomeWork/Homework4Ranges.java
+++ src/HomeWork/Homework4Ranges.java
@@ -9,8 +9,6 @@ public class Homework4Ranges {
         twoRanges();
     }
 
-    //----------------------------------------------
-    //сделать массивами и for перебор
     public static void twoRanges() {
         System.out.println("Сформируйте два интервала\nВведите min и max первого интервала:");
         int[] range1 = inputRange();
@@ -18,32 +16,57 @@ public class Homework4Ranges {
         System.out.println("\nВведите min и max второго интервала:");
         int[] range2 = inputRange();
 
-        setExists(range1);
-        setExists(range2);
+        boolean r1 = setExists(range1);
+        boolean r2 = setExists(range2);
 
+        System.out.println("\n");
+        if(r1 && r2)
+        {
+            rangeRelations(range1, range2);
+        }
+        else
+        {
+            System.out.println("Один из интервалов не существует: проверка вхождения бкссмысленна");
+        }
     }
 
-   /* public static void rangeRelations(int[] range1, int[] range2);
-    {
-        int i = 0;
-        //если min1 < min2 && max1 > max2 - range1 вкл range2
-        //если min1 == min2 && max1 > max2 - range1 вкл range2
-        //если min1 < min2 && max1 == max2 - range1 вкл range2
+    public static void rangeRelations(int[] range1, int[] range2) {
+        int min1 = range1[0];
+        int max1 = range1[1];
 
-        //если min1 > min2 && max1 < max2 - range2 вкл range1
-        //если min1 == min2 && max1 < max2 - range2 вкл range1
-        //если min1 > min2 && max1 == max2 - range2 вкл range1
+        int min2 = range2[0];
+        int max2 = range2[1];
 
-        //если min1 == min2 && max1 == max2 - интервалы идентичны
+        //System.out.println("\n");
 
-        //Пересекает
-        //если min1 > min2 && max1 > max2 - пересекаются
-        //если min1 < min2 && max1 < max2 - пересекаются
-
-        //Не пересекает
-        //если max2 < min1 || min2 > max1 - не пересекаются
-
-    }*/
+        if (min1 > max2 || min2 > max1) {
+            System.out.println("Интервалы [" + min1 + ", " + max1 + "] и [" + min2 + ", " + max2 + "] НЕ пересекаются");
+        } else {
+            if (min1 < min2) {
+                if (max1 >= max2) {
+                    System.out.println("Интервал [" + min1 + ", " + max1 + "] содержит интервал [" + min2 + ", " + max2 + "]");
+                } else {
+                    System.out.println("Интервалы [" + min1 + ", " + max1 + "] и [" + min2 + ", " + max2 + "] пересекаются");
+                }
+            } else if (min1 > min2) {
+                if (max1 <= max2) {
+                    System.out.println("Интервал [" + min2 + ", " + max2 + "] содержит интервал [" + min1 + ", " + max1 + "]");
+                } else {
+                    System.out.println("Интервалы [" + min1 + ", " + max1 + "] и [" + min2 + ", " + max2 + "] пересекаются");
+                }
+            } else if (min1 == min2) {
+                if (max1 > max2) {
+                    System.out.println("Интервал [" + min1 + ", " + max1 + "] содержит интервал [" + min2 + ", " + max2 + "]");
+                } else if (max1 < max2) {
+                    System.out.println("Интервал [" + min2 + ", " + max2 + "] содержит интервал [" + min1 + ", " + max1 + "]");
+                } else {
+                    System.out.println("Интервалы идентичны");
+                }
+            } else {
+                System.out.println("Где-то у меня косяк в расчетах =(");
+            }
+        }
+    }
 
     public static int[] inputRange() {
         int[] range = new int[2];
@@ -53,27 +76,22 @@ public class Homework4Ranges {
         return range;
     }
 
-    public static void setExists(int[] range) {
-        for (int i = 0; i < 1; i++) {
-            if (range[i] > range[i + 1]) {
-                System.out.println("\n");
-                System.out.println(range[i] + " > " + range[i + 1]);
-                System.out.println("Интервал не существует!");
+    public static boolean setExists(int[] range) {
+        int i = 0;
+        System.out.println(Arrays.toString(range));
 
-
-            } else if (range[i] == range[i + 1]) {
-                System.out.println("\n");
-                System.out.println(range[i] + " = " + range[i + 1]);
-                System.out.println("Это не интервал, а точка");
-
-
-            } else {
-                System.out.println("\n");
-                System.out.println(range[i] + " < " + range[i + 1]);
-                System.out.println("Интервал существует");
-
-            }
-            System.out.println(Arrays.toString(range));
+        if (range[i] > range[i + 1]) {
+            // System.out.println(range[i] + " > " + range[i + 1]);
+            System.out.println("Интервал не существует!");
+            return false;
+        } else if (range[i] == range[i + 1]) {
+            //System.out.println(range[i] + " = " + range[i + 1]);
+            System.out.println("Это не интервал, а точка");
+            return true;
+        } else {
+            //System.out.println(range[i] + " < " + range[i + 1]);
+            System.out.println("Интервал существует");
+            return true;
         }
     }
 }
