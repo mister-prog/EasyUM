60946c2a0bffdb8a8977ae22d8534d99e0e590de
mr.Prog <mister_prog@mail.ru>
2020-02-18 20:49:03 +0300

    test

--- src/Lesson5/MainArrays.java
+++ src/Lesson5/MainArrays.java
@@ -1,4 +1,25 @@
 package Lesson5;
 
-public class MainArrays {
+import java.util.Arrays;
+
+public class MainArrays
+{
+    public static void main(String[] args)
+    {
+        int[] a = new int[4];
+
+        System.out.println("a = " + a.toString()); //работает плохо
+        System.out.println("a = " + Arrays.toString(a));
+
+        for(int i = 0; i < a.length; i++)
+        {
+            System.out.println("a[" + i + "] = " + a[i]);
+        }
+
+        int[] b = {1, 3, 7};
+        System.out.println("b = " + Arrays.toString(b));
+
+
+        //System.out.println("args = " + Arrays.toString());
+    }
 }
---
+++ src/Lesson5/MainMax.java
@@ -0,0 +1,8 @@
+package Lesson5;
+
+public class MainMax {
+    public static void main(String[] args) {
+        int[] a = {4, 54, 43, 234, 1};
+
+    }
+}
