07567d30185f1b9fbc8a04be31bcd45aa4329a90
mr.Prog <mister_prog@mail.ru>
2020-02-20 20:15:03 +0300

    arrays: count elements

--- src/lesson6/MainFind.java
+++ src/lesson6/MainFind.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MainFind {
     public static void main(String[] args) {
-        int[] arr = {4, 6, 1, 100, -11, 4, 8, 2, 3};
+        int[] arr = {4, 6, 1, 100, -11, 4, 8, 2, 3, 12, 3, 457, 3, 767, 3, 34, 3, 23, 7};
 
         System.out.println("Index = " + find(arr, -11) + "\nPosition = " + (find(arr,-11) + 1));
         System.out.println("Contains = " + contains(arr, 3));
@@ -12,6 +12,21 @@ public class MainFind {
         System.out.println("Index = " + find(arr, 599) + "\nPosition = " + (find(arr,500) + 1));
         System.out.println("Contains = " + contains(arr, 599));
 
+        System.out.println("count of element = " + countOfElement(arr,3));
+
+    }
+    //сколько раз элемент входит в массив
+    static int countOfElement(int[] arr, int el)
+    {
+        int count = 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] == el)
+            {
+                count++;
+            }
+        }
+        return count;
     }
 
     static boolean contains (int[] arr, int element)
