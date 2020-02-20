2296cab087171cdd543867121fdab6b49bb512c5
mr.Prog <mister_prog@mail.ru>
2020-02-20 19:11:42 +0300

    UI form class added

--- src/lesson5/MainUI.java
+++ src/lesson5/MainUI.java
@@ -1,7 +1,18 @@
 package lesson5;
 
+import javax.swing.*;
+
 public class MainUI {
     public static void main(String[] args) {
+        JTextArea text = new JTextArea(40,40);
+        text.setText("Hello! Put your image here");
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Hello, Java!");
+        frame.setSize(640,480);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(text);
+        frame.setVisible(true);
 
     }
 }
