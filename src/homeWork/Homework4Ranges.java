2dcf93f8b285144d5ff5305b055c3383151a1444
mr.Prog <mister_prog@mail.ru>
2020-02-20 15:39:24 +0300

    Ranges - beta2

--- src/homeWork/Homework4Ranges.java
+++ src/homeWork/Homework4Ranges.java
@@ -19,15 +19,13 @@ public class Homework4Ranges {
     public static void twoRanges() {
         System.out.println("Сформируйте два интервала\nВведите min и max первого интервала:");
         int[] range1 = inputRange();
+        rangeInfo(range1);
 
         System.out.println("\nВведите min и max второго интервала:");
         int[] range2 = inputRange();
+        rangeInfo(range2);
 
-        if ((rangeExists(range1) == RangeStatus.ILLEGAL) || (rangeExists(range2) == RangeStatus.ILLEGAL)) {
-            System.out.println("\nОдин или оба интервала не существуют.\nПоиск пересечений не имеет смысла");
-        } else {
-            rangeRelations(range1, range2);
-        }
+        rangeRelations(range1, range2);
     }
 
     public static void rangeRelations(int[] range1, int[] range2) {
@@ -38,39 +36,46 @@ public class Homework4Ranges {
         int max2 = range2[range2.length - 1];
 
         System.out.println("\n");
-
-        if (min1 > max2 || min2 > max1) {
-            System.out.println("Интервалы " + Arrays.toString(rangePrint(range1)) + " и " + Arrays.toString(rangePrint(range2)) + " НЕ пересекаются");
+        if ((rangeType(range1) == RangeStatus.ILLEGAL) || (rangeType(range2) == RangeStatus.ILLEGAL)) {
+            System.out.println("\nОдин или оба интервала не существуют.\nПоиск пересечений не имеет смысла");
         } else {
-            if (min1 < min2) {
-                if (max1 >= max2) {
-                    System.out.println("Интервал [" + min1 + ", " + max1 + "] содержит интервал [" + min2 + ", " + max2 + "]");
-                } else {
-                    System.out.println("Интервалы [" + min1 + ", " + max1 + "] и [" + min2 + ", " + max2 + "] пересекаются");
-                }
-            } else if (min1 > min2) {
-                if (max1 <= max2) {
-                    System.out.println("Интервал [" + min2 + ", " + max2 + "] содержит интервал [" + min1 + ", " + max1 + "]");
-                } else {
-                    System.out.println("Интервалы [" + min1 + ", " + max1 + "] и [" + min2 + ", " + max2 + "] пересекаются");
-                }
-            } else if (min1 == min2) {
-                if (max1 > max2) {
-                    System.out.println("Интервал ]" + min1 + ", " + max1 + "] содержит интервал [" + min2 + ", " + max2 + "]");
-                } else if (max1 < max2) {
-                    System.out.println("Интервал [" + min2 + ", " + max2 + "] содержит интервал [" + min1 + ", " + max1 + "]");
-                } else {
-                    System.out.println("Интервалы идентичны");
-                }
+            //rangePrint(range1, range2);
+            if (min1 > max2 || min2 > max1) {
+                System.out.println("Интервалы " + rangePrint(range1) + " и " + rangePrint(range2) + " не пересекаются");
             } else {
-                System.out.println("Где-то у меня косяк в расчетах =(");
+                if (min1 < min2) {
+                    if (max1 >= max2) {
+                        System.out.println("Интервал " +rangePrint(range1) + " содержит " + rangePrint(range2));
+                    } else {
+                        System.out.println("Интервалы " + rangePrint(range1) + " и " + rangePrint(range2) + " пересекаются");
+                    }
+                } else if (min1 > min2) {
+                    if (max1 <= max2) {
+                        System.out.println("Интервал " +rangePrint(range2) + " содержит " + rangePrint(range1));
+                    } else {
+                        System.out.println("Интервалы " + rangePrint(range1) + " и " + rangePrint(range2) + " пересекаются");
+                    }
+                } else if (min1 == min2) {
+                    if (max1 > max2) {
+                        System.out.println("Интервал " +rangePrint(range1) + " содержит " + rangePrint(range2));
+                    } else if (max1 < max2) {
+                        System.out.println("Интервал " +rangePrint(range2) + " содержит " + rangePrint(range1));
+                    } else {
+                        System.out.println("Интервалы идентичны");
+                    }
+                } else {
+                    System.out.println("Где-то у меня косяк в расчетах =(");
+                }
             }
         }
-
     }
 
-    public static int[] rangePrint(int[] range) {
-        return range;
+  /*  public static void rangePrint(int[] range1, int[] range2) {
+        System.out.print("Соотношение интервалов " + Arrays.toString(range1) + " и " + Arrays.toString(range2) + ":\n");
+    }*/
+
+    public static String rangePrint(int[] range) {
+        return Arrays.toString(range);
     }
 
     public static int[] inputRange() {
@@ -81,19 +86,32 @@ public class Homework4Ranges {
         return range;
     }
 
-    public static RangeStatus rangeExists(int[] range) {
+    public static RangeStatus rangeType(int[] range) {
         int i = 0;
-        System.out.println(Arrays.toString(range));
-
         if (range[i] > range[range.length - 1]) {
-            System.out.println("Интервал не существует!");
             return RangeStatus.ILLEGAL;
         } else if (range[i] == range[range.length - 1]) {
-            System.out.println("Это не интервал, а точка");
             return RangeStatus.POINT;
         } else {
-            System.out.println("Интервал существует");
             return RangeStatus.LEGAL;
+        }
+    }
+
+    public static void rangeInfo(int[] range) {
+
+        System.out.println(Arrays.toString(range));
+        switch (rangeType(range)) {
+            case LEGAL:
+                System.out.println("Интервал существует");
+                break;
+            case POINT:
+                System.out.println("Это не интервал, а точка");
+                break;
+            case ILLEGAL:
+                System.out.println("Интервал не существует!");
+                break;
+            default:
+                System.out.println("Что-то пошло не так...");
         }
     }
 }
