9175a0c430a59009a332ea6bc22faffb182076b6
mr.Prog <mister_prog@mail.ru>
2020-02-25 20:12:23 +0300

    Foreach

---
+++ src/lesson7/MainForeach.java
@@ -0,0 +1,12 @@
+package lesson7;
+
+public class MainForeach {
+    public static void main(String[] args)
+    {
+        int[] c = {1, 7, 3, 0, 2, 4, 5, 2};
+        for (int el: c) {
+            System.out.println("el = " + el);
+
+        }
+    }
+}
