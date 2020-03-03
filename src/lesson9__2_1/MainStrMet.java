1d01dad326f2d6ed0cb15edca5e8826d94da2577
mr.Prog <mister_prog@mail.ru>
2020-03-03 21:12:46 +0300

    more String methods

--- src/lesson9__2_1/MainStrMet.java
+++ src/lesson9__2_1/MainStrMet.java
@@ -1,11 +1,16 @@
 package lesson9__2_1;
 
+import java.util.Arrays;
+
 public class MainStrMet {
     public static void main(String[] args) {
         String str = "Какой-то длинный неинтересный текст...";
         System.out.println("length = " + str.length());
         System.out.println("str[11] = " + str.charAt(11));
-
         System.out.println("UpperCase = " + str.toUpperCase());
+        System.out.println("lowerCase = " + str.toLowerCase());
+        System.out.println("first index of 'н' = " + str.indexOf('н'));
+        System.out.println("first index of 'W' = " + str.indexOf('W'));
+        System.out.println("words: " + Arrays.toString(str.split(" ")));
     }
 }
