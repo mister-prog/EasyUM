c1f3a3822af8ffa18d027beeddc19626bd889a8a
mr.Prog <mister_prog@mail.ru>
2020-03-12 21:15:25 +0300

    writing own Asserts

---
+++ src/lesson12__2_4/Object/MainObject.java
@@ -0,0 +1,37 @@
+package lesson12__2_4.Object;
+
+public class MainObject {
+    public static void main(String[] args) {
+
+        Person a = new Person("Иванов", 27);
+        Person b = new Person("Иванов", 27);
+        Person c = new Person("Иванов", 27);
+
+        //equals() не работает
+        //вводим доп. условия
+        //! asserts на проде использовать нельзя !
+
+       /* assert a.equals(a) == true; // рефлексия
+        assert a.equals(b) == b.equals(a); // симметричность
+        assert a.equals(c) && b.equals(c) && a.equals(b); //== true;//транзитивность
+        assert a.equals(null) == false;//null
+        //постоянство сравнения во времени, если объекты не менялись*/
+
+
+        assertTrue(a.equals(a) == true, "Нарушена рефлексия"); // рефлексия
+        assertTrue(a.equals(b) == b.equals(a), "Нарушена симметричность"); // симметричность
+        assertTrue(a.equals(c) && b.equals(c) && a.equals(b), "Нарушена транзитивность"); //== true;//транзитивность
+        assertTrue(a.equals(null) == false, "Нулевой объект");//null
+        //постоянство сравнения во времени, если объекты не менялись
+
+        System.out.println("Равны ли? " + a.equals(b));
+    }
+
+    static void assertTrue(boolean condition, String description)
+    {
+        if (!condition)
+        {
+            throw new RuntimeException(description);
+        }
+    }
+}
---
+++ src/lesson12__2_4/Object/Person.java
@@ -0,0 +1,13 @@
+package lesson12__2_4.Object;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
